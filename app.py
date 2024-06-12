5f6c887c05519c82477dd8e70211f6433c5f904a
pau@dev.com <pau@dev.com>
2024-06-12 18:45:50 +0200

    [UPDATE] Last update

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
-from flask_cors import CORS
-import mysql.connector
 import os
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
 
-# Configuración de la base de datos (reemplaza con tus propios datos)
+# Definición de variables de entorno
+db_host = 'localhost'
+db_user = 'root'
+db_password = 'Utx$abeCCrUDnJC)'
+db_database = 'terretaFest'
+
+# Configuración de la base de datos
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'terretaFest'
+    'host': db_host,
+    'user': db_user,
+    'password': db_password,
+    'database': db_database
 }
 
 # Conexión a la base de datos
@@ -28,15 +37,15 @@ def login():
 
     # Verificación de credenciales en la base de datos
     cursor = db_conn.cursor()
-    query = "SELECT username FROM users WHERE email = %s AND password = %s"
+    query = "SELECT username, role_id FROM users WHERE email = %s AND password = %s"
     cursor.execute(query, (email, password))
     user = cursor.fetchone()
     cursor.close()
 
     if user:
-        return jsonify({'username': user[0]})  # Cambiado a devolver el nombre de usuario
+        return jsonify({'username': user[0], 'role_id': user[1]})  # Devolver nombre de usuario y role_id
     else:
-        return jsonify({'error': 'Credenciales incorrectas. Inténtalo de nuevo.', 'email': email, 'password': password})
+        return jsonify({'error': 'Credenciales incorrectas. Inténtalo de nuevo.'})
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -55,7 +64,7 @@ def register():
     db_conn.commit()
     cursor.close()
 
-    return jsonify({'message': 'Usuario registrado exitosamente'})
+    return jsonify({'message': 'Usuari registrat amb èxit'})
 
 @app.route('/productos', methods=['GET'])
 def get_productos():
@@ -79,13 +88,82 @@ def get_productos():
 
     return jsonify(productos_list)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/eventos', methods=['GET'])
+def get_eventos():
+    cursor = db_conn.cursor()
+    query = "SELECT * FROM eventos"
+    cursor.execute(query)
+    eventos = cursor.fetchall()
+    cursor.close()
 
-# Asegúrate de incluir esta condición para que solo se ejecute en el entorno local
+    eventos_list = []
+    for evento in eventos:
+        eventos_list.append({
+            'id': evento[0],
+            'Nombre_del_Evento': evento[1],
+            'Ubicacion': evento[2],
+            'Grupo_de_Musica': evento[3],
+            'N_de_Entradas': evento[4],
+            'Precio': evento[5],
+            'Imagen': evento[6],
+            'Fecha': evento[7],
+            'Informacion': evento[8],
+            'Coordenadas': {
+                'latitud': evento[9],
+                'longitud': evento[10]
+            }
+        })
+
+    return jsonify(eventos_list)
+
+@app.route('/enviar-correo', methods=['POST'])
+def enviar_correo():
+    data = request.json  # Obtener datos JSON del cuerpo de la solicitud
+    nombre = data.get('nombre')
+    correo_usuario = data.get('correo')  # Correo electrónico proporcionado por el usuario
+    asunto = data.get('asunto')
+    mensaje = data.get('mensaje')
+
+    # Configuración del servidor SMTP (reemplaza con tus propios datos)
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_user = 'info.terretafest@gmail.com'  # Tu dirección de correo electrónico
+    smtp_password = 'sdqi tlqp rrqr wqyu'  # Tu contraseña de correo electrónico
+
+    # Construir el mensaje de correo electrónico
+    msg = MIMEMultipart()
+    msg['From'] = smtp_user  # El remitente es info.terretafest
+    msg['To'] = 'terretafestt@gmail.com'  # El destinatario es tu dirección de correo de administración
+    msg['Subject'] = asunto
+    # Contenido del correo con estilo
+    body = f"""
+    <html>
+        <head></head>
+        <body>
+            <p>Salutacions Terretafest,</p>
+            <p>Has rebut un nou missatge de {nombre} ({correo_usuario}):</p>
+            <p>{mensaje}</p>
+            <p>Salutacions,</p>
+            <p>Equipo de Terretafest</p>
+        </body>
+    </html>
+    """
+
+    msg.attach(MIMEText(body, 'html'))
+
+    # Iniciar sesión en el servidor SMTP
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_user, smtp_password)
+
+    # Enviar correo electrónico
+    server.sendmail(smtp_user, 'terretafestt@gmail.com', msg.as_string())
+
+    # Cerrar sesión en el servidor SMTP
+    server.quit()
+
+    return jsonify({'message': 'Correu enviat amb èxit'})
+
 if __name__ == '__main__':
-    # Para Heroku, usa el puerto proporcionado por la variable de entorno PORT
-    port = int(os.environ.get("PORT", 5000))
-    # Quita el parámetro host para que Flask detecte automáticamente el host
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
